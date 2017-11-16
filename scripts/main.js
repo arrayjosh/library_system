dbb5659c48939cf664ab2ad9ee41445ec7bb26c4
Josh Sanderlin <jsanderlin@arrayschool.com>
2017-11-16 14:24:04 -0700

    add book functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 var bookTemplate = $('#templates .book')
+var borrowerTemplate = $('#templates .borrower')
 var bookTable = $('#bookTable')
+var borrowerTable = $('#borrowerTable')
 
 // MY LIBRARY ID IS 128
 
@@ -14,8 +16,24 @@ function addBookToPage(bookData) {
   book.find('.bookTitle').text(bookData.title)
   book.find('.bookImage').attr('src', bookData.image_url)
   book.find('.bookImage').attr('alt', bookData.title)
-  bookTable.append(book)
+  bookTable.prepend(book)
 }
+
+// The borrowerData argument is passed in from the API
+function addBorrowerToPage(borrowerData) {
+  var borrower = borrowerTemplate.clone()
+  borrower.attr('data-id', borrowerData.id)
+  borrower.find('.borrowerFirstName').text(borrowerData.firstname)
+  borrower.find('.borrowerLastName').text(borrowerData.lastname)
+  borrowerTable.prepend(borrower)
+}
+
+/*
+  <tr class="borrower" data-id="some-id">
+    <td class="borrowerFirstName">First Name</td>
+    <td class="borrowerLastName">Last Name</td>
+  </tr>
+*/
 
 var getBooksRequest = $.ajax({
   type: 'GET',
@@ -28,10 +46,35 @@ getBooksRequest.done( (dataFromServer) => {
   })
 })
 
+var getBorrowersRequest = $.ajax({
+  type: 'GET',
+  url: `${baseURL}/borrowers`
+})
 
-/*
-<tr class="book" data-id="some-id">
-  <td><img class="img-responsive bookImage" src="some url" alt="some title"></td>
-  <td class="bookTitle">The Golden Compass</td>
-</tr>
-*/
+getBorrowersRequest.done((dataFromServer) => {
+  dataFromServer.forEach( (borrowerData) => {
+    addBorrowerToPage(borrowerData)
+  })
+})
+
+$('#createBookButton').on('click', () => {
+  var bookData = {}
+
+  bookData.title = $('.addBookTitle').val()
+  bookData.description = $('.addBookDescription').val()
+  bookData.image_url = $('.addBookImageURL').val()
+
+  var createBookRequest = $.ajax({
+    type: 'POST',
+    url: baseURL + '/books',
+    data: {
+      book: bookData
+    }
+  })
+
+  createBookRequest.done((dataFromServer) => {
+    addBookToPage(dataFromServer)
+    $('#addBookModal').modal('hide')
+    $('#addBookForm')[0].reset()
+  })
+})
